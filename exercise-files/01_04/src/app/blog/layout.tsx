d09448b4d35e71b03842ff072a091ac9a707e24e
Thounda Craig <thoundac94@gmail.com>
2025-09-17 16:58:27 -0400

    creating a shared layout between pages

---
+++ exercise-files/01_04/src/app/blog/layout.tsx
@@ -0,0 +1,13 @@
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <h1>Blog</h1>
+      <hr />
+      {children}
+    </>
+  );
+}
