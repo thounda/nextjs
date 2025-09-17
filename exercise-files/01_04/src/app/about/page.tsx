e9e33d78573728b72afb73963278aea78f9bf5f7
Thounda Craig <thoundac94@gmail.com>
2025-09-17 16:22:05 -0400

    create pages and layouts

---
+++ exercise-files/01_04/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p></p>
+      <p>
+        Massa urna magnis dignissim id euismod porttitor vitae etiam viverra
+        nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus
+        pharetra, ac porttitor orci.
+      </p>
+    </>
+  );
+}
