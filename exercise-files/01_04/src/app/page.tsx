62e4b4e5cb36c99bed889f006689055eca7db5f9
Thounda Craig <thoundac94@gmail.com>
2025-09-20 14:35:57 -0400

    style changes to Home component

--- exercise-files/01_04/src/app/page.tsx
+++ exercise-files/01_04/src/app/page.tsx
@@ -5,19 +5,20 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="container mx-auto p-4 relative">
-        <div className="bg-white border-2 border-purple-100 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white border-2 border-emerald-200 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
-            <h1 className={`text-4xl font-bold mb-4 ${styles.text_wisteria}`}>
-              Welcome
+            <h1 className={`text-4xl font-bold mb-4 ${styles.text_green}`}>
+              Nextjs Dynamic Blog Application
             </h1>
             <p className="text-lg text-gray-700 mb-4">
-              Massa urna magnis dignissim id euismod porttitor vitae etiam
-              viverra nunc at adipiscing sit morbi aliquet mauris porttitor
-              nisi, senectus pharetra, ac porttitor orci.
+              This web application was built using Next.Js and deployed to cloud
+              hosting. There is the Home page (current), along with About Us,
+              Post (archieve), and the (dynamically) generated - individual -
+              post pages for view with 'dummy' text.
             </p>
             <a
               href="/blog/posts"
-              className={`outline outline-1 outline-offset-2 border-purple-700 text-purple-700 hover:text-white py-2 px-4 rounded hover:bg-purple-800 md:w-auto ${styles.fit_content}`}
+              className={`outline outline-1 outline-offset-2 border-emerald-300 text-emerald-500 hover:text-white py-2 px-4 rounded hover:bg-emerald-500 md:w-auto ${styles.fit_content}`}
             >
               Go to Blog
             </a>
@@ -44,7 +45,7 @@ export default function Home() {
         </div>
       </div>
       <div
-        className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_wisteria}`}
+        className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_green}`}
       ></div>
     </main>
   );
