d94674c329778776bdc4633db6ebe276a5c233a2
Thounda Craig <thoundac94@gmail.com>
2025-09-29 01:06:38 -0400

    setting up post skeleton template

--- exercise-files/01_04/src/app/blog/posts/loading.tsx
+++ exercise-files/01_04/src/app/blog/posts/loading.tsx
@@ -1,7 +1,13 @@
+import { PostSkeleton } from "@/app/ui/components/skeleton";
+
+// export default function Loading() {
+//   return (
+//     <div className="loading">
+//       <h2>Loading...</h2>
+//     </div>
+//   );
+// }
+
 export default function Loading() {
-  return (
-    <div className="loading">
-      <h2>Loading...</h2>
-    </div>
-  );
+  return <PostSkeleton />;
 }
